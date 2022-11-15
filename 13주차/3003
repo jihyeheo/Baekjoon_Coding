5fbc2c7061da03330d3e764a84ad9c271200de0d
jihye_heo <64202709+jihyeheo@users.noreply.github.com>
2022-11-15 11:56:17 +0900

    20221115

---
+++ 13주차/3003
@@ -0,0 +1,3 @@
+inputs = list(map(int,input().split()))
+
+print(1-inputs[0],1-inputs[1],2-inputs[2],2-inputs[3],2-inputs[4],8-inputs[5])
--- 백준/Bronze/3003. 킹， 퀸， 룩， 비숍， 나이트， 폰/킹， 퀸， 룩， 비숍， 나이트， 폰.py
+++
@@ -1,3 +0,0 @@
-inputs = list(map(int,input().split()))
-
-print(1-inputs[0],1-inputs[1],2-inputs[2],2-inputs[3],2-inputs[4],8-inputs[5])
